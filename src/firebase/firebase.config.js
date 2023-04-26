e56ac79ce804eae1ea77018e367abe800ed5164e
imransarkerweb <imransarker.web@gmail.com>
2023-04-27 00:46:13 +0600

    react bootstrap added and project configured

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCn9mgw_Ks71y9eDECGRJG0rV0uwzkQ5Aw",
+  authDomain: "dragon-news-7e601.firebaseapp.com",
+  projectId: "dragon-news-7e601",
+  storageBucket: "dragon-news-7e601.appspot.com",
+  messagingSenderId: "166627520170",
+  appId: "1:166627520170:web:aef3bc59211667ab932ad3",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
