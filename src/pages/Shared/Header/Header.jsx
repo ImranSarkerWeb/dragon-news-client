a4cdbfc1cfe8ac2377dad4c48c539f530869b681
imransarkerweb <imransarker.web@gmail.com>
2023-04-27 14:57:56 +0600

    Header & momenJs added

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import logo from "../../../assets/logo.png";
+import moment from "moment/moment";
 
 const Header = () => {
-  return <div>this is simple header</div>;
+  return (
+    <div>
+      <div className="text-center">
+        <img src={logo} alt="logo" />
+        <p className="text-secondary mt-3">
+          <small>Journalism Without Fear or Favour</small>
+        </p>
+        <p>
+          {<span className="fw-bold">{moment().format("dddd")} </span>}
+          {moment().format("MMMM D, YYYY")}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
