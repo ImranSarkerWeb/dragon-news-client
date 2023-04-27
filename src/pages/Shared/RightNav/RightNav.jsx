e7d92d06d309ff9c934e974b212a9250245e1a9f
imransarkerweb <imransarker.web@gmail.com>
2023-04-27 16:22:07 +0600

    righ nav updated with React Icons

--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -1,9 +1,39 @@
 import React from "react";
+import { Button, ListGroup } from "react-bootstrap";
+import {
+  FaFacebook,
+  FaGithub,
+  FaGoogle,
+  FaInstagram,
+  FaTwitter,
+} from "react-icons/fa";
 
 const RightNav = () => {
   return (
     <div>
-      <h2>Right Nav upcoming...</h2>
+      <div className="mb-3">
+        <h4>Login With</h4>
+        <Button className="mb-2" variant="outline-primary">
+          <FaGoogle /> Login With Google
+        </Button>
+        <Button variant="outline-secondary">
+          <FaGithub /> Login With Github
+        </Button>
+      </div>
+      <div>
+        <h4>Find Us On</h4>
+        <ListGroup>
+          <ListGroup.Item>
+            <FaFacebook /> facebook
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <FaTwitter /> twitter
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <FaInstagram /> instagram
+          </ListGroup.Item>
+        </ListGroup>
+      </div>
     </div>
   );
 };
