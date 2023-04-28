b91e394f4bdcf54e3f6357f0efbcff817a9e14e3
imransarkerweb <imransarker.web@gmail.com>
2023-04-28 23:27:35 +0600

    category news fetched and style added

---
+++ src/pages/Home/NewsCard/NewsCard.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const NewsCard = ({ news }) => {
+  console.log(news);
+  const { _id, title, details, image_url, author } = news;
+  return (
+    <Card className="mb-4">
+      <Card.Header>Featured</Card.Header>
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Img className="my-4" variant="top" src={image_url} />
+        <Card.Text>
+          {details.length < 250 ? (
+            <>{details}</>
+          ) : (
+            <>
+              {details.slice(0, 300)}...
+              <Link to={`/news/${_id}`}>Read More</Link>
+            </>
+          )}
+        </Card.Text>
+      </Card.Body>
+      <Card.Footer className="text-muted">2 days ago</Card.Footer>
+    </Card>
+  );
+};
+
+export default NewsCard;
